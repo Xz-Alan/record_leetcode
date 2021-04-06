5b848542b072c0c796f298a657ec00fa0d38aef8
xuzhe_nwpu <xzymystery@sina.com>
2021-04-06 21:54:37 +0800

    2021.4.6

--- 39.组合总和.py
+++ 39.组合总和.py
@@ -7,5 +7,24 @@
 # @lc code=start
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def dfs(candidates, begin, size, combine, res, target):
+            if target == 0:
+                res.append(combine)
+                return
+            for index in range(begin, size):
+                diff = target - candidates[index]
+                if diff < 0:
+                    break
+                dfs(candidates, index, size, combine + [candidates[index]], res, diff)
+
+        size = len(candidates)
+        if size == 0:
+            return []
+        candidates.sort()
+        res = []
+        combine = []
+        dfs(candidates, 0, size, combine, res, target)
+        return res
 # @lc code=end
 
--- test.py
+++ test.py
@@ -1,7 +1,20 @@
-nums = [1,2]
-i = 2
-for j in range(2, len(nums)):
-    if nums[j] != nums[i-2]:
-        nums[i] = nums[j]
-        i += 1
-print(nums,i)
+def dfs(candidates, begin, size, combine, res, target):
+    if target == 0:
+        res.append(combine)
+        return
+    for index in range(begin,size):
+        diff = target - candidates[index]
+        if diff < 0:
+            break
+        dfs(candidates, index, size, combine + [candidates[index]], res, diff)
+
+candidates = [2,3,6,7]
+target = 7
+size = len(candidates)
+if size == 0:
+    print([])
+candidates.sort()
+res = []
+combine = []
+dfs(candidates, 0, size, combine, res, target)
+print(res)
