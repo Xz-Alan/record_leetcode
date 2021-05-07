7cfab53c89d33c93ca1b9efd7647c2a3a25778b5
xuzhe_nwpu <xzymystery@sina.com>
2021-05-07 15:27:57 +0800

    5.7打卡

--- 1486.数组异或操作.py
+++ 1486.数组异或操作.py
@@ -7,9 +7,23 @@
 # @lc code=start
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
+        '''
         ans = start
         for i in range(1, n):
             ans ^= (start + i * 2)
         return ans
+        '''
+        def sumXor(x):
+            if x % 4 == 0:
+                return x
+            if x % 4 == 1:
+                return 1
+            if x % 4 == 3:
+                return 0
+            return x+1
+        e = n & start & 1
+        s = start >> 1
+        ans = sumXor(s - 1) ^ sumXor(s + n - 1)
+        return ans << 1 | e
 # @lc code=end
 
