c93903060ba9b1d9538928bbb2c7e3e5a743c3a1
xuzhe_nwpu <xzymystery@sina.com>
2021-05-18 14:52:38 +0800

    5.17打卡

---
+++ 993.二叉树的堂兄弟节点.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=993 lang=python3
+#
+# [993] 二叉树的堂兄弟节点
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        x_parent, x_depth, x_found = None, None, False
+        y_parent, y_depth, y_found = None, None, False
+
+        def dfs(node: TreeNode, depth, parent: TreeNode):
+            if not node:
+                return
+            nonlocal x_parent, x_depth, x_found, y_parent, y_depth, y_found
+
+            if node.val == x:
+                x_parent, x_depth, x_found = parent, depth, True
+            elif node.val == y:
+                y_parent, y_depth, y_found = parent, depth, True
+            
+            if x_found and y_found: return
+
+            dfs(node.left, depth + 1, node)
+
+            if x_found and y_found: return
+
+            dfs(node.right, depth + 1, node)
+        
+        dfs(root, 0, None)
+        return x_depth == y_depth and x_parent != y_parent
+# @lc code=end
+
