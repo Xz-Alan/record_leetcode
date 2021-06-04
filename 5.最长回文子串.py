882aeb9d21f3d4fb62411c99733558abdff84d5f
Xz-Alan <xzymystery@sina.com>
2021-06-04 13:04:31 +0800

    6.4打卡

---
+++ 5.最长回文子串.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode.cn id=5 lang=python3
+#
+# [5] 最长回文子串
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n < 2:
+            return s
+        
+        max_len = 1
+        begin = 0
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        
+        for L in range(2, n + 1):
+            for i in range(n):
+                j = L + i - 1
+                if j >= n:
+                    break
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                else:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    begin = i
+        return s[begin:begin + max_len]
+
+# @lc code=end
+
