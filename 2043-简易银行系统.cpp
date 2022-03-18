92afc907d6b9d2706234f7534f30b71ce58f8da5
Xz-Alan <xzymystery@sina.com>
2022-03-18 11:27:53 +0800

    22.3.18

---
+++ 2043-简易银行系统.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Bank
+{
+private:
+    vector<long long> balance;
+
+public:
+    Bank(vector<long long> &balance) : balance(balance)
+    {
+    }
+
+    bool transfer(int account1, int account2, long long money)
+    {
+        if (account1 > balance.size() || account2 > balance.size() || balance[account1 - 1] < money)
+            return false;
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    bool deposit(int account, long long money)
+    {
+        if (account > balance.size())
+            return false;
+        balance[account - 1] += money;
+        return true;
+    }
+
+    bool withdraw(int account, long long money)
+    {
+        if (account > balance.size() || balance[account - 1] < money)
+            return false;
+        balance[account - 1] -= money;
+        return true;
+    }
+};
+class Bank
+{
+private:
+    vector<long long> balance;
+    long long sz;
+
+public:
+    Bank(vector<long long> &balance)
+    {
+        this->balance = balance;
+        sz = balance.size();
+    }
+
+    bool transfer(int account1, int account2, long long money)
+    {
+        if (account1 > sz || account2 > sz || balance[account1 - 1] < money)
+            return false;
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    bool deposit(int account, long long money)
+    {
+        if (account > sz)
+            return false;
+        balance[account - 1] += money;
+        return true;
+    }
+
+    bool withdraw(int account, long long money)
+    {
+        if (account > sz || balance[account - 1] < money)
+            return false;
+        balance[account - 1] -= money;
+        return true;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
---
+++ 2043-简易银行系统.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+        self.num_acc = len(balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 > self.num_acc or account2 > self.num_acc or self.balance[account1-1] < money:
+            return False
+        self.balance[account1-1] -= money
+        self.balance[account2-1] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account > self.num_acc:
+            return False
+        self.balance[account-1] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account > self.num_acc or self.balance[account-1] < money:
+            return False
+        self.balance[account-1] -= money
+        return True
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
